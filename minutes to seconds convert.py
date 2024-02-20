153574fbbcc7d5db9baea5efce6eba8c33d710a4
GANESH9025 <160560134+GANESH9025@users.noreply.github.com>
2024-02-20 05:37:01 +0000

    Add files via upload

---
+++ minutes to seconds convert.py
@@ -0,0 +1,7 @@
+
+# Python Program to Convert seconds
+# into hours, minutes and seconds
+ 
+Minutes = int(input("Type an amount of time in minutes: "))
+Seconds = Minutes * 60
+print(Minutes, "Minutes =", Seconds, "Seconds")
